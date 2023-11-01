aed565cd1e044f0944ab091accaed326b57f231d
andythebreaker <e24074724@mail.ncku.edu.tw>
2023-11-02 00:17:19 +0800

    mdok

--- example.js
+++ example.js
@@ -1,4 +1,11 @@
+const path = require('path');
+const fs = require('fs');
+
 const main = require('./index');
 
-// You can call the main function to run the Puppeteer script
-main();
+const filesToUpload = ['./img/1.jpg', './img/2.jpg'];
+const outputLocationDir = path.join(process.cwd(), 'output'); // Change this to your desired output location directory
+const pdfFilename = '00e2titl'; // Change this to your desired PDF filename
+
+main(filesToUpload, outputLocationDir, pdfFilename);
+  
--- index.js
+++ index.js
@@ -1,47 +1,61 @@
 const puppeteer = require('puppeteer');
 const path = require('path');
+const fs = require('fs');
+
+// Set the initial file count
+let initialFileCount = 0;
+
+// Function to get the current file count in the directory
+function getCurrentFileCount(outputLocationDir) {
+    const files = fs.readdirSync(outputLocationDir);
+    console.log(files.length)
+    return files.length;
+};
 
 async function main(filesToUpload, outputLocationDir, pdfFilename) {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
 
-  await page.goto('https://andythebreaker.github.io/a4pic2pdf/');
-  const client = await page.target().createCDPSession()
-  await client.send('Page.setDownloadBehavior', {
-    behavior: 'allow',
-    downloadPath: outputLocationDir,
-  })
-  const inputSelector = 'input[type="file"]';
+    await page.goto('https://andythebreaker.github.io/a4pic2pdf/');
+    const client = await page.target().createCDPSession();
+    await client.send('Page.setDownloadBehavior', {
+        behavior: 'allow',
+        downloadPath: outputLocationDir,
+    });
 
-  const [fileInput] = await page.$$(inputSelector);
+    const inputSelector = 'input[type="file"]';
+    const [fileInput] = await page.$$(inputSelector);
 
-  if (fileInput) {
-    await fileInput.uploadFile(...filesToUpload);
+    if (fileInput) {
+        await fileInput.uploadFile(...filesToUpload);
 
-    console.log('Images uploaded.');
+        console.log('Images uploaded.');
 
-    await page.evaluate(newPdfTitle => {
-      document.title = newPdfTitle;
-    }, pdfFilename);
+        await page.evaluate(newPdfTitle => {
+            document.title = newPdfTitle;
+        }, pdfFilename);
 
-    const button = await page.$x("//button[contains(., 'Generate PDF')]");
-    if (button.length > 0) {
-      await button[0].click();
-      console.log('Generate PDF button clicked.');
+        const button = await page.$x("//button[contains(., 'Generate PDF')]");
+        if (button.length > 0) {
+            await button[0].click();
+            console.log('Generate PDF button clicked.');
+        } else {
+            console.error('Generate PDF button not found.');
+        }
     } else {
-      console.error('Generate PDF button not found.');
+        console.error('File input not found.');
     }
-  } else {
-    console.error('File input not found.');
-  }
 
-  await page.waitForTimeout(5000); // Wait for PDF generation to complete (adjust as needed)
-
-  await browser.close();
+    // Check for the new PDF file in the output directory
+    let pdfFilePath;
+    while (true) {
+        const currentFileCount = getCurrentFileCount(outputLocationDir);
+        if (currentFileCount > initialFileCount) {
+            break;
+        }
+        await page.waitForTimeout(1000); // Wait for 1 second before checking again
+    }
+    await browser.close();
 }
 
-const filesToUpload = ['./img/1.jpg', './img/2.jpg'];
-const outputLocationDir = path.join(process.cwd(), 'output'); // Change this to your desired output location directory
-const pdfFilename = '000ftitl'; // Change this to your desired PDF filename
-
-main(filesToUpload, outputLocationDir, pdfFilename);
+module.exports = main;
