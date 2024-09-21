c9942efaa5f5557ce13ba9e4e145ac157e91feb3
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2024-09-21 15:58:23 +0800

    if (!fs.existsSync(path.join(outputLocationDir, pdfFilename))) {

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ const path = require('path');
 const fs = require('fs');
 const fs_ = require('fs').promises;
 
-async function getCurrentFileCount(outputLocationDir) {
+async function getCurrentFileCount(outputLocationDir, silent) {
     try {
         const files = await fs_.readdir(outputLocationDir);
-        console.log(files.length);
+        if (!silent) console.log(files.length);
         return files.length;
     } catch (error) {
-        console.error('An error occurred:', error);
-        return -1; // or any suitable error handling
+        if (!silent) console.error('發生錯誤:', error);
+        return -1; // 或其他適當的錯誤處理
     }
 }
 
-async function main(filesToUpload, outputLocationDir, pdfFilename) {
-    console.log('start')
-    // Set the initial file count
-    let initialFileCount = await getCurrentFileCount(outputLocationDir);
+async function main(filesToUpload, outputLocationDir, pdfFilename, silent = false) {
+    if (!silent) console.log('開始');
+    // 設置初始文件數量
+    let initialFileCount = await getCurrentFileCount(outputLocationDir, silent);
     const browser = await puppeteer.launch({ headless: 'new' });
     const page = await browser.newPage();
 
@@ -34,7 +34,7 @@ async function main(filesToUpload, outputLocationDir, pdfFilename) {
     if (fileInput) {
         await fileInput.uploadFile(...filesToUpload);
 
-        console.log('Images uploaded.');
+        if (!silent) console.log('圖片已上傳。');
 
         await page.evaluate(newPdfTitle => {
             document.title = newPdfTitle;
@@ -43,19 +43,19 @@ async function main(filesToUpload, outputLocationDir, pdfFilename) {
         const button = await page.$x("//button[contains(., 'Generate PDF')]");
         if (button.length > 0) {
             await button[0].click();
-            console.log('Generate PDF button clicked.');
+            if (!silent) console.log('已點擊生成 PDF 按鈕。');
         } else {
-            console.error('Generate PDF button not found.');
+            if (!silent) console.error('未找到生成 PDF 按鈕。');
         }
     } else {
-        console.error('File input not found.');
+        if (!silent) console.error('未找到文件輸入框。');
     }
 
     // Check for the new PDF file in the output directory
     let pdfFilePath;
     while (true) {
-        const currentFileCount = await getCurrentFileCount(outputLocationDir);
-        if (currentFileCount > initialFileCount) {
+        //old logic ~ const currentFileCount = await getCurrentFileCount(outputLocationDir, silent);
+        if (!fs.existsSync(path.join(outputLocationDir, pdfFilename))) {
             break;
         }
         await page.waitForTimeout(1000); // Wait for 1 second before checking again
